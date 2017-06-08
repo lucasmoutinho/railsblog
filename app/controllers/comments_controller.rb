77ae4d3c957d46af651f7c880c2d2cea1bab0784
Lucas Moutinho <lucasmoutinho>
2017-06-08 10:50:34 -0300

    Debugged comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	def create
+  def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
